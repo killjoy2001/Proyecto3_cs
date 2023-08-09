64022346eb0019b98481fc5599c4795802770aaf
Kenny Aragón <kenny.ara.cas.ka@gmail.com>
2023-08-08 21:40:43 -0600

    postres_cs

---
+++ Postres.backend/Postre.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postres.backend
+{
+    public class Postre
+    {
+        /* Definir el nombre ya que es lo que va a diferenciar 
+           los postres y así poder hacer un pullup de la lista correcta */
+
+        public string nombre { get; set; }
+        public string ingrediente { get; set; }
+
+        private LinkedList<string> postre = new LinkedList<string>();
+
+        public void DefinirIngrediente()
+        {
+            Console.WriteLine("Agregue ingrediente");
+            ingrediente = Console.ReadLine();
+            Agregar();
+        }
+
+        private void Agregar()
+        {
+            postre.AddLast(ingrediente);
+        }
+        public void Eliminar()
+        {
+            postre.Remove(ingrediente);
+        }
+        public void Imprimir()
+        {
+            foreach (string item in postre)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Postres.backend/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postres.backend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Postre postre1 = new Postre();
+            Postre postre2 = new Postre();
+            Postre postre3 = new Postre();
+            Postre postre4 = new Postre();
+            Postre postre5 = new Postre();
+            Postre postre6 = new Postre();
+            Postre postre7 = new Postre();
+            Postre postre8 = new Postre();
+            Postre postre9 = new Postre();
+            Postre postre10 = new Postre();
+
+            postre1.DefinirIngrediente();
+
+        }
+    }
+}
