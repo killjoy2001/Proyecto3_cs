524095107fc8f3c2293d5a5d005a9044f7d31468
Kenny Aragón <113489501+killjoy2001@users.noreply.github.com>
2023-08-14 10:44:26 -0600

    Update Postre.cs
    Cambié los métodos y agregué todos en uno solo usando switch

--- Postres.backend/Postre.cs
+++ Postres.backend/Postre.cs
@@ -9,80 +9,64 @@ namespace Postres.backend
     public class Postres
     {
         List<LinkedList<string>> postres = new List<LinkedList<string>>();
-        
-        // need to check this code.
         List<string> nombrePostre = new List<string>();  /* */
         public string nombre {get; set;}
-        
-        public void AgregarPostre()
+        do
         {
-            while (true)
+            switch
             {
-                Console.WriteLine("Ingrese el nombre del postre (o escriba 'salir' para terminar):");
-                /* string nombre = Console.ReadLine(); */
-                nombre = Console.ReadLine();
-
-                if (nombre.ToLower() == "salir")
-                    break;
-
-                LinkedList<string> ingredientes = new LinkedList<string>();
-
-                nombrePostre.Add(nombre); /* */ 
-
-                while (true)
-                {
-                    Console.WriteLine("Ingrese un ingrediente para el postre (o escriba 'fin' para terminar):");
-                    string ingrediente = Console.ReadLine();
-                    Console.Clear();
-
-                    if (ingrediente.ToLower() == "fin")
-                        break;
-
-                    ingredientes.AddLast(ingrediente);
-                }
-
-                postres.Add(ingredientes);
-            }
-        }
-
-        public void Consultar()
-        {
-            Console.WriteLine("\nLista de postres y sus ingredientes:");
-
-            for (int i = 0; i < postres.Count; i++)
-            {
-                /* los datos en las listas nombrePostre e ingredientes van de la mano y
-                los valores comparten el mismo indice. Hay que ejecutar y doublecheck since I'm coding literally on github */
-                    
-                Console.WriteLine(nombrePostre[i]); // imprime el nombre del postre
-                foreach (var ingrediente in postres[i]) // imprime los ingredientes del postre.
-                {
-                    Console.WriteLine("- " + ingrediente);
-                }
-                Console.WriteLine();
-
-            }
-            Console.ReadKey();
-
-        }
-        
-        public void Eliminar()
-        {
-            Console.WriteLine("ingrese el postre a eliminar");
-            string eliminar = Console.ReadLine();
-
-            for (int i = 0; i < postres.Count; i++)
-            {
-                foreach (var postre in nombrePostre[i])
-                {
-                    if (eliminar == nombrePostre[i])
+                case "1": // crea postres y listas con ingredientes 
+                    while (true)
                     {
-                        nombrePostre.RemoveAt(i);
-                        postres.RemoveAt(i);
+                        Console.WriteLine("Ingrese el nombre del postre (o escriba 'salir' para terminar):");
+                        nombre = Console.ReadLine();
+                        if (nombre.ToLower() == "salir")
+                            break;
+                        
+                        LinkedList<string> ingredientes = new LinkedList<string>();
+                        
+                        nombrePostre.Add(nombre); /* */ 
+                        while (true)
+                        {
+                            Console.WriteLine("Ingrese un ingrediente para el postre (o escriba 'fin' para terminar):");
+                            string ingrediente = Console.ReadLine();
+                            Console.Clear();
+                            if (ingrediente.ToLower() == "fin")
+                                break;
+                            ingredientes.AddLast(ingrediente);
+                        }
+                        postres.Add(ingredientes);
+                    } break;
+                    
+                case "2": // imprime las listas
+                    Console.WriteLine("\nLista de postres y sus ingredientes:");
+                    for (int i = 0; i < postres.Count; i++)
+                    {
+                        Console.WriteLine(nombrePostre[i]); // imprime el nombre del postre
+                        foreach (var ingrediente in postres[i]) // imprime los ingredientes del postre.
+                        {
+                            Console.WriteLine("- " + ingrediente);
+                        }
+                        Console.WriteLine();
                     }
-                }
-            }
+                    Console.ReadKey(); break;
 
+                case "3": // eliminar postre
+                    Console.WriteLine("ingrese el postre a eliminar");
+                    string eliminar = Console.ReadLine();
+                    
+                    for (int i = 0; i < postres.Count; i++)
+                    {
+                        foreach (var postre in nombrePostre[i])
+                        {
+                            if (eliminar == nombrePostre[i])
+                            {
+                                nombrePostre.RemoveAt(i);
+                                postres.RemoveAt(i);
+                            }
+                        }
+                    } break;
+            }
         }
     }
 }
